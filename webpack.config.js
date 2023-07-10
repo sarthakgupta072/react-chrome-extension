3748bc34c4a32d5821ae729c17e5650b2d7483ef
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-07-10 18:16:16 +0530

    Basic setup to use react in chrome extension

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 // Uses NodeJs import syntax
-const path = require('path')
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/test.tsx', // tells webpack where to look for files to bundle them. Builds a dependency graph
+    devtool: 'cheap-module-source-map',
+    entry: {
+        popup: path.resolve('src/popup/popup.tsx')
+    }, // tells webpack where to look for files to bundle them. Builds a dependency graph
     module: {
         rules: [
             {
@@ -14,11 +19,26 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve('src/manifest.json'),
+                    to: path.resolve('dist')
+                }
+            ]
+        }),
+        new HtmlPlugin({
+            title: 'React Extension',
+            filename: 'popup.html',
+            chunks: ['popup']
+        })
+    ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
     output: {
-        filename: 'index.js', // File name of the bundled file
+        filename: '[name].js', // File name of the bundled file
         path: path.resolve(__dirname, 'dist'), // dist means distribution
     }
 }
